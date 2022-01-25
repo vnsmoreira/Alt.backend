97b8822d3861aeb1e1b9304b341491550de9f5fa
Vinícius <vnsmoreira.me@gmail.com>
2022-01-24 21:09:10 -0300

    Import search and download routes

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+const searchRoutes = require('./routes/searchRoutes');
+const downloadRoutes = require('./routes/downloadRoutes');
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(require('./routes.js'));
+app.use('/search', searchRoutes);
+app.use('/download', downloadRoutes);
 
 const port = process.env.PORT || 3000;
 
