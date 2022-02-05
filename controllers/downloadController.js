82007b49f31f9a72ab8796b3cacba58597fc6c9d
Vinícius <vnsmoreira.me@gmail.com>
2022-02-05 20:31:25 -0300

    Refactor: change from res.download to res.sendFile

--- controllers/downloadController.js
+++ controllers/downloadController.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 
@@ -5,17 +6,20 @@ const download_video = async (req, res) => {
   const { id } = req.params;
   const videoURL = `https://www.youtube.com/watch?v=${id}`;
 
-  try {
-    const audioPath = `audios/${id}.m4a`;
-    const saveAudio = audio => audio.pipe(fs.createWriteStream(audioPath));
+  const audioPath = `audios/${id}.m4a`;
 
+  const saveAudio = audio => audio.pipe(fs.createWriteStream(audioPath));
+  const removeAudio = () => fs.unlink(audioPath, err => console.log('audio removed.'));
+
+  try {
     const audio = await ytdl(videoURL, { quality: 140 });
+
     saveAudio(audio).on('close', async () => {
-      res.download(`${__dirname}/../${audioPath}`, 'audio.m4a', () => {
-        fs.unlink(audioPath, err => console.log('audio removed.'));
-      });
+      res.sendFile(path.resolve(audioPath), {}, () => removeAudio());
     });
   } catch (error) {
+    removeAudio();
+
     console.log(error);
     res.status(400).send('Not found.');
   }
