ef36fd8f8f3c95cb77ba8981f7528a39cc112982
Vinícius <vnsmoreira.me@gmail.com>
2022-02-05 22:33:55 -0300

    Fix: send content-length header

--- controllers/downloadController.js
+++ controllers/downloadController.js
@@ -1,25 +1,22 @@
-const path = require('path');
-const fs = require('fs');
 const ytdl = require('ytdl-core');
 
 const download_video = async (req, res) => {
   const { id } = req.params;
   const videoURL = `https://www.youtube.com/watch?v=${id}`;
 
-  const audioPath = `audios/${id}.m4a`;
-
-  const saveAudio = audio => audio.pipe(fs.createWriteStream(audioPath));
-  const removeAudio = () => fs.unlink(audioPath, err => console.log('audio removed.'));
-
   try {
     const audio = await ytdl(videoURL, { quality: 140 });
+    let isHeaderSetted = false;
 
-    saveAudio(audio).on('close', async () => {
-      res.sendFile(path.resolve(audioPath), {}, () => removeAudio());
-    });
+    audio
+      .on('progress', (chunk, loaded, total) => {
+        if (!isHeaderSetted) {
+          res.set('content-length', total);
+          isHeaderSetted = true;
+        }
+      })
+      .pipe(res);
   } catch (error) {
-    removeAudio();
-
     console.log(error);
     res.status(400).send('Not found.');
   }
